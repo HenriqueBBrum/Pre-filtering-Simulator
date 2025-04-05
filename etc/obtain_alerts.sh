1529745e35c530f5a66f9d41469d988250ebd2b2
Henrique <hbrum499@gmail.com>
2025-04-05 08:10:30 +0000

    Updated CICIoT2023 pcaps

--- etc/obtain_alerts.sh
+++ etc/obtain_alerts.sh
@@ -1,13 +1,13 @@
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <dataset_name>"
-    echo "Dataset name must be either CICIoT2023 or CICIDS2017"
+    echo "Dataset name must be either CICIDS2017 or CICIoT2023"
     exit 1
 fi
 
 dataset_name=$1
 
-if [ "$dataset_name" != "CICIoT2023" ] && [ "$dataset_name" != "CICIDS2017" ]; then
-    echo "Invalid dataset name. Must be either CICIoT2023 or CICIDS2017"
+if [ "$dataset_name" != "CICIDS2017" ] && [ "$dataset_name" != "CICIoT2023" ]; then
+    echo "Invalid dataset name. Must be either CICIDS2017 or CICIoT2023"
     exit 1
 fi
 
@@ -28,9 +28,13 @@ for pcap in $pcap_folder*.pcap; do
     name=${pcap##*/}
     name=${name%%.*}
 
-    snort -c $snort_conf_file --rule-path "${script_dir}/rules/snort3-registered/" -r $pcap -l $log_dir -A alert_json --lua "alert_json = {file = true}"
-    mv "${log_dir}/alert_json.txt" "${log_dir}/snort/${name}.txt"
+    if [[ "$name" != "DoS-HTTP_Flood" ]]; then
+        continue
+    fi
 
-    suricata  -c $suricata_conf_file -S "${script_dir}/rules/suricata-emerging/emerging-all.rules" -r $pcap -l $log_dir
-    mv "${log_dir}/fast.log" "${log_dir}/suricata/${name}.log"
+    snort -c $snort_conf_file --rule-path "${script_dir}/rules/snort3-registered/" -r $pcap -l $log_dir -A alert_json --lua "alert_json = {file = true}"
+    mv "${log_dir}/alert_json.txt" "${log_dir}/snort/${name}.log"
+
+    # suricata  -c $suricata_conf_file -S "${script_dir}/rules/suricata-emerging/emerging-all.rules" -r $pcap -l $log_dir
+    # mv "${log_dir}/fast.log" "${log_dir}/suricata/${name}.log"
 done
